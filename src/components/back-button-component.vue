1ed3af9fc3b618da0edba48e67ff6c98580330ef
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2023-12-16 04:06:25 +0700

    correction

---
+++ src/components/back-button-component.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+<template>
+  <div class="back-btn-comp"><router-link :to="`/`"><div class="back-btn"></div></router-link></div>
+</template>
+<style>
+.back-btn{
+  background: url("/src/assets/left-arrow-svgrepo-com.svg")no-repeat;
+  display: inline-block;
+  width: 30px;
+  height: 20px;
+  margin-top: 30px;
+}
+</style>
---
+++ src/pages/news-list/news-list-page.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+const news = ref([])
+const selectedArticle = ref(null)
+const comments = ref([])
+
+
+const fetchNews = async () => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/v0/topstories.json`)
+  const storyIds = await response.json()
+  const stories = await Promise.all(storyIds.slice(0, 100).map(async (id) => {
+    const storyResponse = await fetch(`${import.meta.env.VITE_API_URL}/v0/item/${id}.json`)
+    return await storyResponse.json()
+  }))
+  return stories
+}
+
+
+const fetchComments = async (articleId) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/v0/item/${articleId}.json`)
+  const article = await response.json()
+  // Assuming the comments are stored in the "kids" property of the article
+  comments.value = article.kids
+}
+
+const showComments = (article) => {
+  selectedArticle.value = article
+  fetchComments(article.id)
+}
+
+
+
+const sortByDate = (articles) => {
+  return articles.sort((a, b) => b.time - a.time)
+}
+
+onMounted(async () => {
+  // Fetch news and sort by date
+  news.value = sortByDate(await fetchNews())
+})
+setInterval(function(){
+  location.reload();
+}, 60000);
+
+
+</script>
+
+<template>
+
+  <ul class="news-list">
+    <li v-for="article in news" :key="article.id" class="article">
+
+      <router-link :to="`/news/${article.id}`" target="_blank">
+        <h2>{{ article.title }}</h2>
+      </router-link>
+      <p>{{article.score}} points | by: {{article.by}}</p>
+
+    </li>
+  </ul>
+
+</template>
